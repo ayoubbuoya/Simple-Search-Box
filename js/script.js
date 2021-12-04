fd72e63ac967dce6817839b75efff9b6897c1c71
Ayoub Amer <69482481+ayoubbuoya@users.noreply.github.com>
2021-12-04 01:23:23 -0800

    JavaScript File

---
+++ js/script.js
@@ -0,0 +1,6 @@
+var searchBox = document.getElementById("search-box");
+var googleIcon = document.getElementById("google-icon");
+googleIcon.onclick = function() {
+    searchBox.classList.toggle("show");
+     
+};
